3338019f0afc9b766e42794a9c614baa3357d85b
vishnuvardhanpattem <97816318+vishnuvardhanpattem@users.noreply.github.com>
2024-03-10 10:02:49 +0530

    Update CorsConfig.java

--- src/main/java/com/vishnu/movies/CorsConfig.java
+++ src/main/java/com/vishnu/movies/CorsConfig.java
@@ -9,14 +9,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("http://localhost:3000", "https://splendorous-sundae-37c6f1.netlify.app/"); // Replace with the actual origin of your React app
-		config.addAllowedMethod("*");
-		config.addAllowedHeader("*");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://splendorous-sundae-37c6f1.netlify.app");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
